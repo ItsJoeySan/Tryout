c1697f624c764d6263cfb7df3e01125c74ce60a2
ItsJoey <ItsJoeySan@gmail.com>
2024-11-23 19:20:41 +0530

    fixed quill error

--- app/quillEditor/page.tsx
+++ app/quillEditor/page.tsx
@@ -1,10 +1,16 @@
 "use client";
-import QuillEditor from "../components/QuillEditor";
+// import QuillEditor from "../components/QuillEditor";
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(() => import("../components/QuillEditor"), {
+  ssr: false,
+});
 
 const editor = () => {
   return (
     <div className="flex flex-col justify-center items-center ">
-      <QuillEditor />
+      {/* <QuillEditor /> */}
+      <ReactQuill />
     </div>
   );
 };
